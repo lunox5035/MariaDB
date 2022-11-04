73acf6b17162f2fcfd40cf931223a3636804539f
GawnGyu Yeo <bbc02131@naver.com>
2022-11-04 12:32:08 +0900

    Bookshop#

---
+++ JDBC_practices/src/main/java/com/bitacademy/bookshop/vo/AuthorVo.java
@@ -0,0 +1,23 @@
+package com.bitacademy.bookshop.vo;
+
+public class AuthorVo {
+	private Long no;
+	private String name;
+	@Override
+	public String toString() {
+		return "AuthoVo [no=" + no + ", name=" + name + "]";
+	}
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
